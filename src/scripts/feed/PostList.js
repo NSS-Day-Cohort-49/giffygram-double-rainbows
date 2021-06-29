9ecdfe648f235835035e07f50d5be97227b98188
Jalon Duncan <duncanjalon@gmail.com>
2021-06-29 14:09:23 -0500

    finished ticket 9

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -8,24 +8,23 @@ export const postFeed = ()=> {
 	const usersName = getUsers()
 	let html = `<button id="to_new_post_page_button"> Create New Post </button> <section class="post_feed_wrapper">`
 	currentPost.map((post)=>{ 
-
-
+	const postName = usersName.find((user) => {
+		if (user.id === post.userId){
+			return user.name
+		}
+	})
+	
 		return html += `
 			<div class="post_wrapper">
 			<h3> ${post.title}</h3>
 			<img class="post_gif" src="${post.imageURL}" alt="${post.title}"> 
 			<div> ${post.description} </div>
-			<div> Posted by 
-				<div class="userNameLink" id="targetUser--${post.userId}"> ${usersName.find((name) => {
-					if (name.id === post.userId){
-						return name.name
-					}})
-				}</div>
-			<div> at ${new Date(post.timeStamp)} </div>
+			<div> Posted by <div class="userNameLink" id="targetUser--${post.userId}"> ${postName.name}</div>
+			<div id="output"> at ${new Date(post.timeStamp)} </div>
 		`
 	}).join("")
-	 html += `</section>`
-	 return html
+	html += `</section>`
+	return html
 	
 }
 
