2786d85ce5d01dc5626badc1246760336bc79039
joshuastewart417 <joshua.stewart780@gmail.com>
2021-06-29 15:55:09 -0500

    added image html within postFeed function in PostList.js

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,9 +1,11 @@
-import { getPosts } from "../data/provider.js"
+import { getPosts, getLikes } from "../data/provider.js"
 import { newPostForm } from "./PostForm.js"
 import { userProfile } from "./UserProfile.js"
 const applicationElement = document.querySelector(".giffygram")
 
 export const postFeed = ()=> {
+	const likes = getLikes()
+	const currentUserId = parseInt(localStorage.getItem("gg_user"))
 	const currentPost = getPosts()
 	const sortedPost = currentPost.sort((a,b)=> {return b.timeStamp-a.timeStamp})
 	let html = `<button id="to_new_post_page_button"> Create New Post </button> <section class="post_feed_wrapper">`
@@ -16,8 +18,13 @@ export const postFeed = ()=> {
 			<img class="post_gif" src="${post.imageURL}" alt="${post.title}"> 
 			<div> ${post.description} </div>
 			<div> Submitted by <div class="userNameLink" id="targetUser--${post.userId}"> ${post.userId}</div> at
-			 ${post.timeStamp} </div>
-			 </div>
+			${post.timeStamp} </div>
+			<div class="favorite_wrapper">
+			<button onclick="document.getElementById('favorite_button').src='./images/favorite-star-yellow.svg'">
+		    <img class="star_button" id="favorite_button" src="./images/favorite-star-blank.svg">
+			<button onclick="document.getElementById('favorite_button').src='./images/favorite-star-blank.svg'">
+			</div>
+			</div>
 		`
 	}).join("")
 	 html += `</section>`
